2335c7e367aa82b2360f47c14f18a1d084387939
wangpei <wangpei@asipreadtech.com>
2018-10-24 17:41:56 +0800

    修改公共头部样式

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,21 @@
 <template>
 	<div id="header">
-		<div id="title">MINI-OA</div>
-		<div class="function-icons">
-			<div class=""></div>
+		<div id="title" class="pull-left">MINI-OA</div>
+		<div class="function-icons pull-left">
+			<el-button size="small" type="info" icon="el-icon-edit" circle></el-button>
+      <el-button size="small" type="info" icon="el-icon-edit" circle></el-button>
+      <el-button size="small" type="info" icon="el-icon-edit" circle></el-button>
 		</div>
+    <div class="user-info pull-right">
+      <div class="user-icon pull-left"><img src="../assets/images/user.jpg" alt="用户头像"></div>
+      <div class="user-name pull-left">UserInfo</div>
+    </div>
+    <div class="clearfix"></div>
 	</div>
 </template>
 <style scoped lang="scss">
 	#header{
+    height: 60px;
 		padding: 0 20px;
 		background-color:#545c64;
 		#title{
@@ -16,11 +24,29 @@
 			width: 195px;
 			height:60px;
 			line-height: 60px;
-			float:left;
 		}
 		.function-icons{
 			height: 60px;
 			line-height:60px;
-		}
+    }
+    .user-info{
+      height:60px;
+      .user-icon{
+        height: 60px;
+        margin: auto;
+        img{
+          width: 40px;
+          height: 40px;
+          margin-top: 10px;
+          border-radius: 20px;
+        }
+      }
+      .user-name{
+        line-height: 60px;
+        width: 100px;
+        text-align: center;
+        color: #FFFFFF;
+      }
+    }
 	}
 </style>
